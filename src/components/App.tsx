274178f6088fd02acb536b2624f1471671d13c52
Robert Kirsz <robert.kirsz@gmail.com>
2022-03-14 20:11:27 +0100

    Add comments

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,8 @@ export default function App() {
   const { dispatch } = useContext(ProgressIndicatorsContext)
 
   const spawnProgress = () => {
+    // This will happen, for example, when user uploads a file.
+    // We may pass here and ID we get from the backend and some other metadata.
     dispatch({ type: 'create' })
   }
 
--- src/components/ProgressIndicators.tsx
+++ src/components/ProgressIndicators.tsx
@@ -21,31 +21,35 @@ function Item({ id, status, percentage }: ProgressIndicatorType) {
   const { dispatch } = useContext(ProgressIndicatorsContext)
 
   const cancel = () => {
+    // Here we should also inform the backend that the upload should be cancelled.
+    // We may also have separate actions for cancelling the upload and clicking "Done" button.
     dispatch({ type: 'cancel', payload: { id } })
   }
 
   useEffect(() => {
     if (status === 'success') {
-      timeoutRef.current = window.setTimeout(() => {
-        setIsFinished(true)
-      }, 1000)
-
+      timeoutRef.current = window.setTimeout(() => setIsFinished(true), 1000)
       return
     }
 
     if (percentage === 100) {
+      // Instead of waiting for progress to be 100, we can also wait for the backend to respond with status 'success'.
+      // But for this proof of concept we need to mock that response.
       dispatch({ type: 'updateStatus', payload: { id, status: 'success' } })
 
       return
     }
 
     if (status === 'pending') {
-      timeoutRef.current = window.setTimeout(() => {
-        dispatch({ type: 'updatePercentage', payload: { id, percentage: randomInt() } })
-      }, 500)
+      // Instead of this mocked timeout here, we would subscribe to some backend event.
+      timeoutRef.current = window.setTimeout(
+        () => dispatch({ type: 'updatePercentage', payload: { id, percentage: randomInt() } }),
+        500
+      )
     }
   }, [id, status, percentage, dispatch])
 
+  // We need to clear the timeout on unmount.
   useEffect(() => () => clearTimeout(timeoutRef.current), [])
 
   return (
